2f7419e6ddf4490e97527d26f72b1d4a5a4b1c12
dmx <dingmingxin20@gmail.com>
2018-04-16 16:50:56 +0800

    toolbar work now

--- Classes/GHSettingWindowControler.m
+++ Classes/GHSettingWindowControler.m
@@ -9,20 +9,33 @@
 #import "GHSettingWindowControler.h"
 #import "GHDefaultManager.h"
 #import "Constant.h"
+#import "GHProfileViewController.h"
+
+#define ID_PROFILES @"Profiles"
+#define ID_SYNC @"Sync"
+#define ID_ABOUT @"About"
 
 @interface GHSettingWindowControler ()
+
 @property NSMutableDictionary *toolBarItems;
 @property NSMutableArray *toolbarIdentifiers;
+//@property NSInteger selectedPaneTag;
+@property NSMutableDictionary *controllers;
+@property NSDictionary *controllerIdMap;
+
 - (void)toolbarItemSelected:(id)sender;
+- (void)initToolbar;
+- (void)showView:(NSString *)identifier;
+-(NSRect) frameRectWithPin:(NSPoint)point andContentSize:(NSSize)size;
+
 @end
 
 @implementation GHSettingWindowControler
 @synthesize toolBarItems, toolbarIdentifiers;
 
-- (void)awakeFromNib {
-    
+- (void)initToolbar {
     NSMutableDictionary *items = [[NSMutableDictionary alloc] initWithCapacity:1];
-    NSArray *identifiers = @[@"General", @"Sync", @"About"];
+    NSArray *identifiers = @[ID_PROFILES, ID_SYNC, ID_ABOUT];
     
     NSInteger tag = 0;
     for (NSString *identifier in identifiers) {
@@ -35,12 +48,15 @@
         [items setObject:item forKey:identifier];
         tag += 1;
     }
-
+    
     self.toolBarItems = items;
+    
+    //identifiers
     self.toolbarIdentifiers = [NSMutableArray arrayWithArray:identifiers];
     [self.toolbarIdentifiers insertObject:NSToolbarFlexibleSpaceItemIdentifier atIndex:0];
     [self.toolbarIdentifiers insertObject:NSToolbarFlexibleSpaceItemIdentifier atIndex:[self.toolbarIdentifiers count]];
     
+    //init toolbar
     NSToolbar *toolbar = [[NSToolbar alloc] initWithIdentifier:@"GhoskSKBPreferenceWindowToolbar"];
     [toolbar setAllowsUserCustomization:NO];
     [toolbar setAutosavesConfiguration:NO];
@@ -48,12 +64,65 @@
     [toolbar setDelegate:self];
     [toolbar setSelectedItemIdentifier:[self.toolbarIdentifiers objectAtIndex:1]];
     [self.window setToolbar:toolbar];
+    
+    //initial viewcontroller
+    [self showView:ID_PROFILES];
+}
 
+- (void)awakeFromNib {
+    self.controllerIdMap = [[NSDictionary alloc] initWithObjectsAndKeys:
+                            @"ProfileViewController", ID_PROFILES,
+                            @"InfoViewController", ID_ABOUT,
+                            @"SyncViewController", ID_SYNC,
+                            nil];
+    [self initToolbar];
 }
 
 - (void)toolbarItemSelected:(id)sender {
+    NSToolbarItem* item = sender;
     
+    NSInteger tag = [item tag];
+//    if (tag == self.selectedPaneTag) {
+//        return;
+//    }
+//    self.selectedPaneTag = tag;
+    NSString *identifier = item.itemIdentifier;
+    [self showView:identifier];
+    NSLog(@"selected Tab:%@", identifier);
 }
+
+- (void)showView:(NSString *)identifier {
+    NSViewController *controller = [self.controllers objectForKey:identifier];
+    if (controller == NULL) {
+        NSStoryboard *board = [NSStoryboard storyboardWithName:@"Storyboard" bundle:nil];
+        NSString *storyboarId = [self.controllerIdMap objectForKey:identifier];
+        controller = [board instantiateControllerWithIdentifier:storyboarId];
+    }
+
+    NSView *newView = controller.view;
+    NSSize newSize = [newView frame].size;
+    NSRect newFrame = [self frameRectWithPin:NSZeroPoint andContentSize:newSize];
+    
+    [newView setFrameOrigin:NSZeroPoint];
+    [newView setAutoresizingMask:NSViewMaxYMargin | NSViewWidthSizable | NSViewMinXMargin | NSViewMaxXMargin];
+
+    [self.window setTitle:identifier];
+    [self.window center];
+    [self.window setContentView:newView];
+    [self.window setFrame:newFrame display:YES animate:YES];
+    [self.controllers setObject:controller forKey:identifier];
+}
+
+
+- (NSRect)frameRectWithPin:(NSPoint)point andContentSize:(NSSize)size
+{
+    NSRect oldFrame = [self.window frame];
+    NSRect newFrame = [self.window frameRectForContentRect:NSMakeRect(0,0,size.width,size.height)];
+    newFrame.origin.x = (oldFrame.origin.x + oldFrame.size.width / 2.0) - newFrame.size.width / 2.0;
+    newFrame.origin.y = (oldFrame.origin.y + oldFrame.size.height) - newFrame.size.height;
+    return newFrame;
+}
+
 
 - (void)windowDidLoad {
     [super windowDidLoad];
